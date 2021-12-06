0e8f999ea5a98479b73f5d3aa9e40121e370f073
Daniel Wykerd <danielwykerd@gmail.com>
2021-12-06 12:00:57 +0200

    fix: macOS byteswap

--- src/ws.c
+++ src/ws.c
@@ -7,7 +7,15 @@
 #include <openssl/err.h>
 #if defined(__linux__)
 #  include <endian.h>
-#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(APPLE)
+#elif defined(__APPLE__)
+#  include <machine/endian.h>
+#  define be16toh(x) OSSwapBigToHostInt16(x)
+#  define be32toh(x) OSSwapBigToHostInt32(x)
+#  define be64toh(x) OSSwapBigToHostInt64(x)
+#  define htobe16(x) OSSwapHostToBigInt16(x)
+#  define htobe32(x) OSSwapHostToBigInt32(x)
+#  define htobe64(x) OSSwapHostToBigInt64(x)
+#elif defined(__FreeBSD__) || defined(__NetBSD__)
 #  include <sys/endian.h>
 #elif defined(__OpenBSD__)
 #  include <sys/types.h>
